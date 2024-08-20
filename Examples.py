1616127375d30324f9434c88e108587816543b7a
chaitanyakumar30071997 <142807369+chaitanyakumar30071997@users.noreply.github.com>
2024-08-20 11:29:25 +0530

    Pandas-Notes

---
+++ Examples.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data =  {
+     ' laxmi' : [23,87,47,96,96,87 ],
+     'pri':        [46,86,85,48,75,58],
+     'sneha' : [87,57,96,96,58,47]
+  }
+
+df = pd.DataFrame(data)
+
+print(df)
